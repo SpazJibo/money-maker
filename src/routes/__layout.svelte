6e7da9ffbded0235c9d07f0b116e05294b24da5f
Curtis Allen Gibeaut Jr <curtjibo@gmail.com>
2022-01-15 15:03:29 -0600

    Initial Creation of Money Maker Demo App.

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="bg-gradient-to-b from-green-400 to-orange-400 h-screen">
+	<slot />
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,32 @@
+<div class="grid grid-cols-1 md:grid-cols-2 gap-3 p-3">
+	<div class="bg-white rounded p-3 col-span-1 md:col-span-2">
+		Adipisicing vero quis consectetur quae fugiat A impedit distinctio eos repudiandae optio.
+		Fugiat explicabo laudantium esse unde alias Molestiae facilis ratione tenetur velit possimus
+		Ea ut blanditiis soluta mollitia enim? Aliquid architecto placeat corporis nesciunt atque!
+		Sed debitis reprehenderit dignissimos suscipit quasi. Consequatur perferendis optio
+		voluptates autem impedit. Totam praesentium similique optio obcaecati hic Saepe itaque dicta
+		esse sed officia. Quo nesciunt architecto amet nesciunt laboriosam Vel voluptatem vel
+		corporis hic iure. Adipisci amet quos reiciendis veritatis error id? Iste iste culpa
+		deleniti maxime commodi Ab voluptatem facilis laborum itaque pariatur, inventore. Ut a sequi
+		accusamus expedita doloribus? Repellendus nemo mollitia incidunt facilis accusantium! Culpa
+		veniam quos voluptatibus suscipit est Explicabo facilis perferendis voluptate tenetur
+		necessitatibus. Commodi quae illo ullam saepe a, facere officia Dicta accusantium fuga qui
+		corrupti eligendi Amet blanditiis perspiciatis accusantium officia animi. Neque consectetur
+		soluta illo ullam ullam! Voluptas dignissimos accusamus reiciendis velit voluptatum?
+		Temporibus aliquid
+	</div>
+	<div class="bg-white rounded p-3">
+		Amet laudantium at ipsa dolor odio dolor Numquam sint veniam eius saepe excepturi
+		voluptatibus dignissimos ullam, neque sapiente unde Consequatur quos doloribus accusantium
+		accusamus id! Magni incidunt expedita amet amet quos accusamus Minus eaque esse iure
+		laudantium rerum enim Beatae dolor doloremque nobis obcaecati nemo Voluptate quam assumenda
+		facilis reiciendis.
+	</div>
+	<div class="bg-white rounded p-3">
+		Consectetur quod provident impedit neque cumque Fugiat molestias temporibus commodi
+		temporibus eius. Dicta velit modi voluptatum quidem reiciendis. Fugiat ipsam aspernatur
+		voluptatem quidem impedit, obcaecati. Ducimus dignissimos praesentium commodi unde deleniti
+		tempora! Deserunt magnam nostrum placeat labore maxime. Repellendus velit soluta
+		exercitationem nemo esse fugiat! Officiis lorem nihil sapiente asperiores
+	</div>
+</div>
